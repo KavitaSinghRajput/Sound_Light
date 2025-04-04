89acd812aa250c6342c9e3d42bb103a569172fde
KavitaSinghRajput <dev.kavita20@gmaial.com>
2025-04-04 12:12:14 +0530

    aaa

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,34 @@
-import React from "react";
+import React ,{useState} from "react";
+import {Link} from "react-router-dom";
 import "./Navbar.css"; // Import CSS for styling
 import logo from "../../../src/assets/Logo1.png";  // Correct path
  // Replace with your logo path
 
 const Navbar = () => {
+  const [menuOpen,setMenuOpen]=useState(false);
+
   return (
     <nav className="navbar">
-    <div className="navbar-container"></div>
+    <div className="navbar-container">
+      <button className="menu-btn" onClick={()=>setMenuOpen(!menuOpen)}>
+      ☰
+      </button>
+    </div>
       
-      <ul className="nav-links left-links">
-        <li className="me-5 ho" ><a  href="#Home">Home</a></li>
-        <li className="me-5 ho" ><a href="#Services">Services</a></li>
-        <li className="me-5 ho" ><a href="#Events">Events</a></li>
+      <ul className={menuOpen ?"nav-links open" : "nav-links left-links"}>
+
+        <li><Link to="/">Home</Link> </li>
+        <li><Link to="/services">Services</Link> </li>
+        <li><Link to="/events">Events</Link> </li>
+
        </ul>
         <div className="logo-container">
         <img src={logo} alt="Indore Sound & Light Logo" className="logo" />
       </div>
       <ul className="nav-links left-links">
-        <li className="me-5 ho" ><a href="#Equipment">Equipment</a></li>
-        <li className="me-5 ho" ><a href="#Contact">Contact</a></li>
-      </ul>
+      <li><Link to="/equipment">Equipment</Link> </li>
+      <li><Link to="/contact">Contact</Link> </li>
+     </ul>
     </nav>
   );
 };
